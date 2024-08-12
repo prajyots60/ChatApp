91913a2f46298b8f89c8907a3a6127a7bba21115
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-08-12 19:21:25 +0530

    get users

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
 
@@ -28,6 +30,7 @@ app.use(cookieParser());  //parse cookies from the client
 
 app.use("/api/auth" ,authRoutes);
 app.use("/api/messages" ,messageRoutes);
+app.use("/api/users" ,userRoutes);
 
 //listen 
 app.listen(PORT, () => {
