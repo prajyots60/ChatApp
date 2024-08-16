352ce5cf8746fe9eac8c7441782696bb9adf26f2
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-08-17 02:11:22 +0530

    ERROR RESOLVE

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,6 +1,6 @@
 import Message from "./Message";
 import  useGetMessages  from "../../hooks/useGetMessages";
-import MessageSkeleton from "../skeletons/messageSkeleton";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import { useEffect , useRef } from "react";
 import useListenMessages from "../../hooks/useListenMessages";
 
